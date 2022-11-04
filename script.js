c1e454edf84913fbb9a7610987c6d93aca3cacdd
JGabss <jgabriel.abreum@gmail.com>
2022-11-04 09:38:16 -0300

    Subindo projeto

---
+++ script.js
@@ -0,0 +1,40 @@
+const bottom = document.getElementById("buttom-sub");
+
+var form = document.getElementById("form");
+function handleForm(e) { event.preventDefault(); } 
+form.addEventListener('submit', handleForm);
+
+
+bottom.addEventListener("click", () => {
+    let n1 = parseInt(document.getElementById("n1").value);
+    let n2 = parseInt(document.getElementById("n2").value);
+    let n3 = parseInt(document.getElementById("n3").value);
+    let n4 = parseInt(document.getElementById("n4").value);
+
+    if (!(nome = "" || n1 == "" || n2 == "" || n3 == "" || n4 == "" )) {
+
+        let mean = (n1 + n2 + n3 + n4) / 4
+        
+        console.log(nome.value)
+        if (mean > 69) {
+            swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: `Parabéns, ${document.getElementById("nome").value}, APROVADO(A)!`,
+                text: `Média: ${mean}`,
+            })
+        } else if (mean < 40) {
+            swal.fire({
+                icon: 'error',
+                title: `F, ${document.getElementById("nome").value}, reprovado(a)!`,
+                text: `Média: ${mean}`,
+            }) 
+        } else {
+            swal.fire({
+                icon: 'warning',
+                title: `Ainda há esperanças, ${document.getElementById("nome").value}, você irá fazer final`,
+                text: `Média: ${mean}`,
+            })
+        }
+    } 
+})
